1ba336e59f1e19cc19ae749dde4d6e06854fe7f7
Alabi Bolaji <alabibolaji78@gmail.com>
2021-09-19 18:23:14 +0100

    completed challenge

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float passedTime;
+
+
+    private void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        passedTime += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (passedTime >= 1.0f)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                passedTime = 0;
+            }
+        }
+
+    }
+
+    void getKey()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horInput;
+    public float speed = 10.0f;
+    public int range = 20;
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -range )
+        { transform.position = new Vector3(-range, transform.position.y, transform.position.z); } 
+        if(transform.position.x > range)
+        { transform.position = new Vector3(range, transform.position.y, transform.position.z); }
+
+        horInput = Input.GetAxis("Horizontal");
+        transform.Translate(horInput * speed * Time.deltaTime * Vector3.right);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        { Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation); }
+    }
+}
